b03acf44234933f39d40a65a8c256b5dd55dd90b
Olivia Argent <olivia.j.argent@outlook.com>
2018-08-12 16:44:08 +0100

    cmd  added

--- Dance.py
+++ Dance.py
@@ -3,9 +3,18 @@
 
 # Import Python elements
 from rrb3 import *
+import sys
+import tty
+import termios
 
 # Set any aliases
-rr = RRB3()
+rr = RRB3(9.0, 6.0) # (battery, motor)
+
+# Commands
+UP = 0
+DOWN = 1
+RIGHT = 2
+LEFT = 3
 
 # Set Default Exit Massage or Instructions
 print("Press CTRL-c to quit the program")
